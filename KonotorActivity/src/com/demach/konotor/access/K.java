7a932279d81750154bbe263537275200e5fd23bd
Deepak Bala <deepak.balu@gmail.com>
2013-12-11 10:38:08 +0530

    initial commit
    initial commit

---
+++ KonotorActivity/src/com/demach/konotor/access/K.java
@@ -0,0 +1,127 @@
+package com.demach.konotor.access;
+
+import com.demach.R;
+
+public class K
+{
+    public static final class attr {
+    }
+    public static final class color {
+        public static int light_gray= R.color.light_gray;
+        public static int progress_end=R.color.progress_end;
+        public static int progress_start=R.color.progress_start;
+        public static int voice_feedback_progress_end=R.color.voice_feedback_progress_end;
+        public static int voice_feedback_progress_start=R.color.voice_feedback_progress_start;
+        public static int white= R.color.white;
+        public static int message_read= R.color.message_read;
+    }
+    public static final class drawable {
+        public static int empty_photo=R.drawable.empty_photo;
+        public static int ic_launcher=R.drawable.ic_launcher;
+        public static int info=R.drawable.info;
+        public static int message_read_transition=R.drawable.message_read_transition;
+        public static int nav_100=R.drawable.nav_100;
+        public static int nav_prev=R.drawable.nav_prev;
+        public static int placeholder_profile=R.drawable.placeholder_profile;
+        public static int play_green=R.drawable.play_green;
+        public static int read_background_drawable=R.drawable.read_background_drawable;
+        public static int seek_drawable=R.drawable.seek_drawable;
+        public static int seekbar_thumb_small=R.drawable.seekbar_thumb_small;
+        public static int speakeroff=R.drawable.speakeroff;
+        public static int speakeron=R.drawable.speakeron;
+        public static int stop_yellow=R.drawable.stop_yellow;
+        public static int toast_frame=R.drawable.toast_frame;
+        public static int unread_background_drawable=R.drawable.unread_background_drawable;
+        public static int voice_feedback_drawable = R.drawable.voice_feedback_drawable;
+        public static int chat = R.drawable.chat;
+        
+        public static int upload = R.drawable.upload;
+        public static int tick = R.drawable.tick;
+    }
+    public static final class id {
+        public static int cancel_recording_bt=R.id.cancel_recording_bt;
+        public static int cancel_relative_layout=R.id.cancel_relative_layout;
+        public static int contactimage=R.id.contactimage;
+        public static int conv_child_container=R.id.conv_child_container;
+        public static int conv_heading=R.id.conv_heading;
+        public static int conversation_haed_list=R.id.conversation_haed_list;
+        public static int download_progress=R.id.download_progress;
+        public static int downloading_your_message=R.id.downloading_your_message;
+        public static int m_message_reply_text_bt=R.id.m_message_reply_text_bt;
+        public static int m_message_reply_voice_bt=R.id.m_message_reply_voice_bt;
+        public static int m_message_send_bt=R.id.m_message_send_bt;
+        public static int messageText=R.id.messageText;
+        public static int message_back_bt=R.id.message_back_bt;
+        public static int message_m_duration=R.id.message_m_duration;
+        public static int message_m_time=R.id.message_m_time;
+        public static int message_play_bt=R.id.message_play_bt;
+        public static int message_play_progress=R.id.message_play_progress;
+        public static int nav_m_relative_layout=R.id.nav_m_relative_layout;
+        public static int photo_imageview=R.id.photo_imageview;
+        public static int photo_rel_container=R.id.photo_rel_container;
+        public static int relative_layout_reply=R.id.relative_layout_reply;
+        public static int relative_layout_reply_inner=R.id.relative_layout_reply_inner;
+        public static int relative_m_inner_layout=R.id.relative_m_inner_layout;
+        public static int speakerToggle_img=R.id.speakerToggle_img;
+        public static int sticker_imageview=R.id.sticker_imageview;
+        public static int sticker_rel_container=R.id.sticker_rel_container;
+        public static int sticker_webview=R.id.sticker_webview;
+        public static int textView1=R.id.textView1;
+        public static int time_elapsed=R.id.time_elapsed;
+        public static int toast_layout_root=R.id.toast_layout_root;
+        public static int toast_text=R.id.toast_text;
+        public static int user_m_name=R.id.user_m_name;
+        public static int v_send_bt=R.id.v_send_bt;
+        public static int voice_feedback_progress=R.id.voice_feedback_progress;
+        public static int message_subject_text=R.id.message_subject_text;
+        public static int upload_status = R.id.upload_status;
+    }
+    public static final class layout {
+        public static int conv_child_row = R.layout.conv_child_row;
+        public static int conv_child_row_right = R.layout.conv_child_row_right;
+        public static int message_screen_layout = R.layout.message_screen_layout;
+        public static int toast_layout = R.layout.toast_layout;
+        public static int voice_feedback = R.layout.voice_feedback;
+        public static int user_name_layout = R.layout.user_name_layout;
+    }
+    public static final class string {
+        public static int _00=R.string._00;
+        public static int a_very_long_message_text=R.string.a_very_long_message_text;
+        public static int app_name=R.string.app_name;
+        public static int compose_new_message=R.string.compose_new_message;
+        public static int conv_id=R.string.conv_id;
+        public static int downloading=R.string.downloading;
+        public static int duration=R.string.duration;
+        public static int go_back=R.string.go_back;
+        public static int no_network_connection_toast=R.string.no_network_connection_toast;
+        public static int recording=R.string.recording;
+        public static int response_with_voice=R.string.response_with_voice;
+        public static int send_message=R.string.send_message;
+        public static int time=R.string.time;
+        public static int toggle_speaker=R.string.toggle_speaker;
+        public static int you=R.string.you;
+    }
+    public static final class style {
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+         */
+        public static int AppBaseTheme=R.style.AppBaseTheme;
+        /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+         */
+        public static int AppTheme=R.style.AppTheme;
+    }
+    
+    public static final class raw {
+        public static int roboto_light = R.raw.roboto_light;
+    }
+    
+    public static final String ANDROID_PROJECT_SENDER_ID = "114180086868";
+}
